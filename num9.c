c207b9ea166f65ffb190f39154219853da9b0999
c-dager <dagerc@protonmail.com>
2024-10-25 14:12:43 -0400

    FInish num9

---
+++ num9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void rollDice() {
+    int num = (rand() % 6)+ 1;;
+    printf("You rolled a: %d\n", num);
+}
+
+int main() {
+    char* keepPlaying = "y";
+    char userInput[50];
+    while(1){
+        rollDice();
+        printf("Enter y to keep playing\n");
+        scanf("%s", userInput);
+        if(strcmp(keepPlaying, userInput) == 0){
+            continue;
+        }
+        else{
+            break;
+        }
+    }
+    printf("Thanks for playing!\n");
+
+}
